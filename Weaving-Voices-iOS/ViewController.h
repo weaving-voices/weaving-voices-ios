631707657942b4e93efa4cd0cbb9fabe6f61e752
Panos Pandis <panospad@gmail.com>
2014-05-11 14:25:11 +0300

    Initial Commit

---
+++ Weaving-Voices-iOS/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Weaving-Voices-iOS
+//
+//  Created by Panos Pandis on 5/11/14.
+//  Copyright (c) 2014 Panos Pandis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Weaving-Voices-iOSTests/Weaving_Voices_iOSTests.m
@@ -0,0 +1,34 @@
+//
+//  Weaving_Voices_iOSTests.m
+//  Weaving-Voices-iOSTests
+//
+//  Created by Panos Pandis on 5/11/14.
+//  Copyright (c) 2014 Panos Pandis. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Weaving_Voices_iOSTests : XCTestCase
+
+@end
+
+@implementation Weaving_Voices_iOSTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
